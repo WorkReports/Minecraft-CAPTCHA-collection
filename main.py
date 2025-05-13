03379abf55d791eae4eec1a8b631c55dc6684cc2
Vladimir <118909455+WorkReports@users.noreply.github.com>
2025-05-13 23:58:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, WebSocket
+import uvicorn
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        print(f"Получено: {data}")
+        await websocket.send_text(f"Ответ: {data}")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=10000)
